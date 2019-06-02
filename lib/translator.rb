dc71ce306deea6e89a904750a06efab1619148e2
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-02 15:54:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,24 @@
 # require modules here
+require "yaml"
 
-def load_library
+
+def load_library(path)
   # code goes here
+ emoticons = YAML.load_file(path)
+ new_emoticons = {}
+ new_emoticons['get_meaning'] = {}
+ new_emoticons['get_emoticon'] = {}
+ 
+  emoticons.each do |meaning, characters|
+    new_emoticons['get_meaning'][characters[1]] = meaning
+    new_emoticons['get_emoticon'][characters[0]] = characters[1]
+  end
+new_emoticons
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
